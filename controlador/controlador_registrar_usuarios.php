34bc2c7d7dadc3b9ae2c6178357671bb97e75183
Adamz <91496585+GiovanniAz@users.noreply.github.com>
2024-05-17 00:56:40 -0500

    controlador_registrar_usuarios.php

--- controlador/controlador_registrar_usuarios.php
+++ controlador/controlador_registrar_usuarios.php
@@ -1,10 +1,10 @@
 <?php
-include("conexion.php"); // Incluye el archivo de conexión
+include("conexion.php"); 
 
-// Inicializa la variable mensaje
+
 $mensaje = "";
 
-// Verificar si se envió el formulario
+
 if (isset($_POST['register'])) {
     if (empty($_POST['usuario'])  || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['telefono'])) {
         $mensaje = '<div class="Alerta">Uno de los campos está vacío</div>';
@@ -18,14 +18,12 @@ if (isset($_POST['register'])) {
 
         if ($conn->query($sql) === TRUE) {
             echo '<div class="Alerta">Usuario Registrado Correctamente</div>';
-            // Limpiar los campos del formulario después de un registro exitoso
             $_POST['usuario'] = '';
             $_POST['email'] = '';
             $_POST['password'] = '';
             $_POST['telefono'] = '';
-            // Redirigir al usuario a la página de login
             header('Location: ../login/login.php');
-            exit; // Asegúrate de salir del script después de redirigir
+            exit; 
         } else {
             echo '<div class="Alerta">Error al registrar</div>';
         }
