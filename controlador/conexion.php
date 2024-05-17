6bf970d9fcd3d46a5bc4bb3ad88b41365bf9876d
Adamz <91496585+GiovanniAz@users.noreply.github.com>
2024-05-17 00:56:12 -0500

    conexion.php

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "credenciales";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
